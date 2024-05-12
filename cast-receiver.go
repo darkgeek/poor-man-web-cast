8f4a212da7ce5b720d83495bdf05f434a93252ba
Justin Yang <linuxjustin@gmail.com>
2024-05-12 21:03:13 +0800

    init commit

---
+++ cast-receiver.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os/exec"
+)
+
+func call(exe string, params ...string) {
+    cmd := exec.Command(exe, params...)
+    stdout, err := cmd.Output()
+    
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+
+    // Print the output
+    fmt.Println(string(stdout))
+}
+
+func getParamFromReq(req *http.Request, paramName string) string {
+	value := req.FormValue(paramName)
+	if len(value) == 0 {
+		fmt.Println("value not present")
+		return ""
+	} else {
+		fmt.Println("get value: " + value)
+		return value
+	}
+}
+
+func openFirefoxInKioskMode(w http.ResponseWriter, req *http.Request) {
+	urlToOpen := getParamFromReq(req, "url")
+	call("firefox", "--kiosk", urlToOpen)
+}
+
+func openMpv(w http.ResponseWriter, req *http.Request) {
+	urlToOpen := getParamFromReq(req, "url")
+	call("mpv", "-fs", "--ytdl-format=ytdl", "--ytdl-raw-options=cookies-from-browser=chromium", urlToOpen)
+}
+
+func openChromiumInKioskMode(w http.ResponseWriter, req *http.Request) {
+	urlToOpen := getParamFromReq(req, "url")
+	call("chromium-browser", "--kiosk", urlToOpen)
+}
+
+func main() {
+
+    http.HandleFunc("/firefox-kiosk", openFirefoxInKioskMode)
+    http.HandleFunc("/mpv", openMpv)
+    http.HandleFunc("/chromium-kiosk", openChromiumInKioskMode)
+
+    http.ListenAndServe(":8090", nil)
+}
