402efc665d6d5715355df27aebb017870db839f9
Justin Yang <linuxjustin@gmail.com>
2024-05-13 17:18:17 +0800

    add expire time for notification

--- cast-receiver.go
+++ cast-receiver.go
@@ -54,21 +54,21 @@ func getParamFromReq(req *http.Request, paramName string) string {
 func openFirefoxInKioskMode(w http.ResponseWriter, req *http.Request) {
 	urlToOpen := getParamFromReq(req, "url")
 	call("killall", "-9", "firefox")
-	call("notify-send", "正在启动firefox...")
+	call("notify-send", "-t", "3000", "正在启动firefox...")
 	callAsync("firefox", "--kiosk", urlToOpen)
 }
 
 func openMpv(w http.ResponseWriter, req *http.Request) {
 	urlToOpen := getParamFromReq(req, "url")
 	call("killall", "-9", "mpv")
-	call("notify-send", "正在解析资源并启动mpv播放器...")
+	call("notify-send", "-t", "3000", "正在解析资源并启动mpv播放器...")
 	callAsync("./run-mpv.sh", urlToOpen)
 }
 
 func openChromiumInKioskMode(w http.ResponseWriter, req *http.Request) {
 	urlToOpen := getParamFromReq(req, "url")
 	call("killall", "-9", "/usr/lib/chromium-browser/chromium-browser")
-	call("notify-send", "正在启动chromium...")
+	call("notify-send", "-t", "3000", "正在启动chromium...")
 	callAsync("chromium-browser", "--kiosk", urlToOpen)
 }
 
